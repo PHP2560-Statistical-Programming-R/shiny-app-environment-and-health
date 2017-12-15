f40265302133e50ff411fa0f9bbe1ce794905315
Annie Yang <xiaofei_yang@brown.edu>
2017-12-15 14:39:37 -0500

    change input name

--- App_combine_version/combine.R
+++ App_combine_version/combine.R
@@ -109,7 +109,7 @@ ui <- navbarPage("Temperatures and AQI",
                             tabPanel("Air Quality Map",
                               fluidPage(headerPanel("AirQuality"),
                                                           sidebarPanel(
-                                                          numericInput("year", "Year", 2005, min = 1999, max = 2013), #Choosing year you want to observe
+                                                          numericInput("yearaqm", "Year", 2005, min = 1999, max = 2013), #Choosing year you want to observe
                                                           radioButtons("color","Color",choices = c('Blues','Reds','Purples'), 
                                                                          selected = 'Purples') #Choosing different state background color
                                                           ),
@@ -372,7 +372,7 @@ server <- function(input, output) {
   })
   
   source("R/statemap.R")
-  output$map <- renderPlotly(checkAirQuality(data = AirQuality_Tracking,year=input$year, 
+  output$map <- renderPlotly(checkAirQuality(data = AirQuality_Tracking,year=input$yearaqm, 
                                              Color = input$color))
 }
 
